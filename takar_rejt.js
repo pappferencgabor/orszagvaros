abeb08f4a9f348a6b6f4dddc3cd241e43377ca36
pappferencgabor <70635774+pappferencgabor@users.noreply.github.com>
2020-12-06 16:56:46 +0100

    alapok

---
+++ takar_rejt.js
@@ -0,0 +1,336 @@
+function showpw() {
+    var pw = document.getElementById('showpw');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw1() {
+    var pw = document.getElementById('showpw1');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw2() {
+    var pw = document.getElementById('showpw2');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw3() {
+    var pw = document.getElementById('showpw3');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw4() {
+    var pw = document.getElementById('showpw4');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw5() {
+    var pw = document.getElementById('showpw5');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw6() {
+    var pw = document.getElementById('showpw6');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw7() {
+    var pw = document.getElementById('showpw7');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw8() {
+    var pw = document.getElementById('showpw8');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw9() {
+    var pw = document.getElementById('showpw9');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw10() {
+    var pw = document.getElementById('showpw10');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw11() {
+    var pw = document.getElementById('showpw11');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw12() {
+    var pw = document.getElementById('showpw12');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw13() {
+    var pw = document.getElementById('showpw13');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw14() {
+    var pw = document.getElementById('showpw14');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw15() {
+    var pw = document.getElementById('showpw15');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw16() {
+    var pw = document.getElementById('showpw16');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw17() {
+    var pw = document.getElementById('showpw17');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw18() {
+    var pw = document.getElementById('showpw18');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw19() {
+    var pw = document.getElementById('showpw19');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw20() {
+    var pw = document.getElementById('showpw20');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw21() {
+    var pw = document.getElementById('showpw21');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw22() {
+    var pw = document.getElementById('showpw22');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw23() {
+    var pw = document.getElementById('showpw23');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw24() {
+    var pw = document.getElementById('showpw24');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw25() {
+    var pw = document.getElementById('showpw25');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw26() {
+    var pw = document.getElementById('showpw26');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw27() {
+    var pw = document.getElementById('showpw27');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw28() {
+    var pw = document.getElementById('showpw28');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw29() {
+    var pw = document.getElementById('showpw29');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw30() {
+    var pw = document.getElementById('showpw30');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw31() {
+    var pw = document.getElementById('showpw31');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw32() {
+    var pw = document.getElementById('showpw32');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw33() {
+    var pw = document.getElementById('showpw33');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw34() {
+    var pw = document.getElementById('showpw34');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw35() {
+    var pw = document.getElementById('showpw35');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw36() {
+    var pw = document.getElementById('showpw36');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw37() {
+    var pw = document.getElementById('showpw37');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw38() {
+    var pw = document.getElementById('showpw38');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw39() {
+    var pw = document.getElementById('showpw39');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw40() {
+    var pw = document.getElementById('showpw40');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
+function showpw41() {
+    var pw = document.getElementById('showpw41');
+
+    if (pw.type == "text")
+        pw.type = "password";
+    else
+        pw.type = "text";
+}
